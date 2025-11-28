timtbh/ephinea-character-viewer-overlay

commit 3ba36a0ffa56ca14109bda634070abc4f2dbf06a

show meseta in inventory + pmt update + bikeshedding

src/read.js
@@ -167,7 +167,6 @@ const parseWeapon_ = (match, hex, name, special) =>
    , hit      : +match[10]
    , untekked : !!match[11]
    , kills    : +match[12] || 0
-   , rare     : pmt[hex].rarity >= 9
    , quantity : 1
    , category : "Weapon"
    , hex      : hex

src/utils.js
@@ -29,7 +29,7 @@ export const chunksOf = k => xs => {
 }
 
 export const range = (x, y, step = 1) =>
-  x > y ? [] : [x, ...range(x + step, y)]
+  x > y ? [] : [x, ...range(x + step, y, step)]
 
 export const keysEqual = (o1, o2, ...keys) =>
   keys.every(key => o1[key] === o2[key])

css/style.css
@@ -99,6 +99,10 @@
   .inventory {
     margin-top: -5px;
   }
+
+  .inventory-meseta {
+    margin-top: 5px;
+  }
   
   .link-arrow {
     margin-left: 20px;

src/components/item.js
@@ -56,7 +56,7 @@ const showName = (item, unitxt) => {
   // skinned?
   const suffix = item.name.match(/\*$/) ? "*" : ""
   return (
-    pmt[item.hex].rarity < 9
+    pmt[item.hex].stars < 9
       ? `<span class="common-name ${cssIdentifierFrom(item.name)}">
            ${unitxt.items[item.hex]}
          </span>`

src/components/main-view.js
@@ -50,7 +50,7 @@ const showMeseta = (meseta) => {
   return `
     <div class="inventory-meseta">
       ${"&nbsp;".repeat(4)}
-      <span class="meseta">◆ ${formatted}</span>
+      <span class="meseta">${formatted} ◆</span>
     </div>`
 }
 

src/show-text.js
@@ -25,7 +25,7 @@ const showGrind = item =>
   item.grind ? `+${item.grind} ` : ""
 
 const showSpecial = (item, settings) =>
-  item.special && (!item.rare || settings.showRareSpecials)
+  item.special && (pmt[item.hex].stars < 9 || settings.showRareSpecials)
     ? `[${unitxt.en.specials[item.special]}] `
     : ""
 

commit 912ded19d655489693c177f94c1a342c7e1bf4bf

Fixed bug introduced in 3ba36a0 that prevented common vs rare weapons from being sorted and stacked correctly

src/main.js
@@ -47,7 +47,7 @@ const sortAndStack = items => {
     return { ...ungrouped, slot: idx + 1 }
   }
 
-  return (groupBy (itemsEqual) (items.toSorted(compareItems))).map(flatten)
+  return groupBy(itemsEqual, items.toSorted(compareItems)).map(flatten)
 }
 
 const withStackedBank = viewer => {
@@ -81,7 +81,7 @@ const addItemGroups = viewer => {
     ]
     .toSorted(compareItems)
 
-  const grouped = (groupBy (sameType) (everything)).map(sortAndStack)
+  const grouped = groupBy(sameType, everything).map(sortAndStack)
 
   const labels =
     { Common   : "Common Weapons"

src/read.js
@@ -166,6 +166,7 @@ const parseWeapon_ = (match, hex, name, special) =>
    , hit      : +match[10]
    , untekked : !!match[11]
    , kills    : +match[12] || 0
+   , rare     : pmt[hex].stars >= 9
    , quantity : 1
    , category : "Weapon"
    , hex      : hex

src/utils.js
@@ -1,4 +1,4 @@
-export const groupBy = cmp => xs => {
+export const groupBy = (cmp, xs) => {
   const { acc, cur } =
     xs.slice(1).reduce(({ x, acc, cur }, y) =>
       cmp(x, y)
@@ -13,7 +13,7 @@ export const groupBy = cmp => xs => {
   return [...acc, cur]
 }
 
-export const chunksOf = k => xs => {
+export const chunksOf = (k, xs) => {
   const { acc, cur } =
     xs.reduce(({ acc, cur, len }, x) =>
       len == k

tools/genplt.lua
@@ -22,19 +22,15 @@ local function readU8(str, offset)
 end
 
 local function readU16(str, offset)
-  return bit.bor(
-    bit.lshift(str:byte(offset + 2),  8),
-               str:byte(offset + 1)
-  )
+    return str:byte(offset + 2) * 2 ^ 8
+         + str:byte(offset + 1)
 end
 
 local function readU32(str, offset)
-  return bit.bor(
-    bit.lshift(str:byte(offset + 4), 24),
-    bit.lshift(str:byte(offset + 3), 16),
-    bit.lshift(str:byte(offset + 2),  8),
-               str:byte(offset + 1)
-  )
+    return str:byte(offset + 4) * 2 ^ 24
+         + str:byte(offset + 3) * 2 ^ 16
+         + str:byte(offset + 2) * 2 ^ 8
+         + str:byte(offset + 1)
 end
 
 local function readArray(str, offset, read, n, sizeof)